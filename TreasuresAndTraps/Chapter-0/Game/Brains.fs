faf2a58ed95fa9d4dd6bc944ff0483cf9fac2a72
henry.keen <henry.keen@just-eat.com>
2014-11-23 12:35:04 +0000

    Make it random

--- TreasuresAndTraps/Chapter-0/Game/Brains.fs
+++ TreasuresAndTraps/Chapter-0/Game/Brains.fs
@@ -10,5 +10,11 @@ TODO: implement random decision making
 The Creature can go in either direction, 
 with equal probability.
 *)
+    let r = new Random()
 
-    let randomDecide () = Straight
+    let randomDecide () = 
+        let direction = r.Next(3)
+        match direction with
+        | 0 -> Straight
+        | 1 -> Right
+        | _ -> Left
